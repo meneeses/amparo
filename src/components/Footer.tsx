089f053a945f7592206e8a70e87c53c5b3a6aca6
João Meneses <meneses-joao@hotmail.com>
2026-04-14 16:06:12 -0300

    feat: add Footer component to display logo, slogan, and copyright information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer({ dict }: { dict: any }) {
   return (
     <footer>
-      <span className="footer-logo">{dict.footer.slogan.split(' ')[0]}</span>
+      <span className="footer-logo">{dict.nav.logo}</span>
       <p>{dict.footer.slogan}</p>
       <p style={{ marginTop: '1.5rem' }}>{dict.footer.copy}</p>
     </footer>
